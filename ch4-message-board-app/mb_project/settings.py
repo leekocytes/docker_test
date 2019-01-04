2fef5ccdc3b05c80913a471e3d2cf44c0fb33e85
Lee Rodgers <leerrodgers@gmail.com>
2019-01-04 13:23:31 -0600

    Making compatiable with Heroku production

--- ch4-message-board-app/mb_project/settings.py
+++ ch4-message-board-app/mb_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import whitenoise
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '()mdj2!mf7hqdm3d%+sv5b1xa&t+9vae026c08z-*&2!rl$77b'
+SECRET_KEY ='()mdj2!mf7hqdm3d%+sv5b1xa&t+9vae026c08z-*&2!rl$77b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mb_project.urls'
@@ -77,9 +80,10 @@ WSGI_APPLICATION = 'mb_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db', # set in docker-compose.yml
+        'NAME': 'ddscbluae8rnc5',
+        'USER': 'mfdhtihomdddrf',
+        'PASSWORD': '25aab99b2b7b9514b125cafa9033fcae12dd97964e05ac0f8a0e06a954bed483',
+        'HOST': 'ec2-54-225-227-125.compute-1.amazonaws.com', # set in docker-compose.yml
         'PORT': 5432 # default postgres port
     }
 }
@@ -121,4 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
