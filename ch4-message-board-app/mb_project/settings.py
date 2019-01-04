6bf19107bb1f9a12bbaf7235f3adf76e86a8fecd
Lee Rodgers <leerrodgers@gmail.com>
2019-01-04 15:20:16 -0600

    Seperating .env for local and prod runs.

--- ch4-message-board-app/mb_project/settings.py
+++ ch4-message-board-app/mb_project/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='()mdj2!mf7hqdm3d%+sv5b1xa&t+9vae026c08z-*&2!rl$77b'
+#SECRET_KEY ='()mdj2!mf7hqdm3d%+sv5b1xa&t+9vae026c08z-*&2!rl$77b'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
